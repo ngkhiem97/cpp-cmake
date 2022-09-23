26ffaa81e0714c83c1ba8581f6664a12b7ec13cf
Khiem Nguyen <ng.khiem97vn@gmail.com>
2022-09-23 04:03:22 -0400

    update

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <cmath>
+#include <iostream>
 #include "MathFunctions.h"
 
 // a hack square root calculation using simple operations
@@ -9,21 +11,12 @@ double mysqrt(double x)
     return 0;
     }
 
-  double result;
-  double delta;
-  result = x;
-
-  // do ten iterations
-  int i;
-  for (i = 0; i < 10; ++i)
-    {
-    if (result <= 0)
-      {
-      result = 0.1;
-      }
-    delta = x - (result*result);
-    result = result + 0.5*delta/result;
-    fprintf(stdout,"Computing sqrt of %g to be %g\n",x,result);
-    }
+  #if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = std::exp(std::log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+              << " using log and exp" << std::endl;
+  #else
+    double result = x;
+  #endif
   return result;
 }
